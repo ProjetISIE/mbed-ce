4579bebab90feff4ae23d0698eac3f8be77719cf
Guilhem Fauré <pro@guilhemfau.re>
2026-03-12 10:52:43 +0100

    better flake

--- flake.nix
+++ flake.nix
@@ -1,51 +1,77 @@
 {
-  description = "Nix flake C/C++ development environment";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; # NixOS Stable
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+  description = "Nix flake MBED development environment";
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
     { self, nixpkgs }:
     let
-      systems =
+      localSystems = [
+        "x86_64-linux" # "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs localSystems (system: f (import nixpkgs { inherit system; }));
+      forAllSystemsWithCross =
         f:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          # "aarch64-darwin"
-        ] (system: f { pkgs = import nixpkgs { inherit system; }; });
+        let
+          crossSystem = "aarch64-linux";
+        in
+        nixpkgs.lib.genAttrs localSystems (
+          localSystem:
+          f (import nixpkgs { inherit localSystem; }) (
+            import nixpkgs {
+              inherit localSystem;
+              inherit crossSystem;
+            }
+          )
+        );
     in
     {
-      devShells = systems (
-        { pkgs }:
-        {
-          default =
-            pkgs.mkShell.override
-              {
-                stdenv = pkgs.clangStdenv; # Clang instead of GCC
-              }
-              {
-                packages = with pkgs; [
-                  clang-tools # Clang CLIs
-                  cmake # Automation tool
-                  cmake-language-server # LSP
-                  cppcheck # Static analysis
-                  # clang-uml # Generate UML from c(++)
-                  doctest # Testing framework
-                  doxygen # Documentation generator
-                  # gdb # Debugger
-                  # gtest # Testing framework
-                  lcov # Code coverage
-                  lldb # Debug adapter
-                  # pkg-config # Find libraries
-                  valgrind # Debugging and profiling
-                ];
-                env = {
-                  CXX = "c++"; # Use Clang++ as the default C++ compiler
-                  CC = "clang"; # Use Clang as the default C compiler
-                  CXXFLAGS = "-std=c++23 -Wall -Wextra -Wpedantic -Wshadow -Wconversion";
-                };
-              };
-        }
-      );
+      # packages = forAllSystems (pkgs: {
+      #   default = pkgs.callPackage ./pkg.nix {
+      #     inherit self pkgs;
+      #     stdenv = pkgs.clangStdenv;
+      #   };
+      # });
+      # crossPackages = forAllSystemsWithCross (
+      #   pkgs: crossPkgs: {
+      #     default = crossPkgs.callPackage ./pkg.nix {
+      #       inherit self;
+      #       stdenv = crossPkgs.clangStdenv;
+      #       pkgs = crossPkgs;
+      #     };
+      #   }
+      # );
+      devShells = forAllSystems (pkgs: {
+        default =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.clangStdenv; # Clang instead of GCC
+            }
+            {
+              packages = with pkgs; [
+                bashInteractive
+                clang-tools # Clang CLIs, including LSP
+                clang-uml # UML diagram generator
+                cmake-format # CMake formatter
+                cmake-language-server # Cmake LSP
+                doxygen # Documentation generator
+                lldb # Clang debug adapter
+                mbed-cli
+                socat # Serial terminal for manual testing
+                valgrind # Debugging and profiling
+              ];
+              # nativeBuildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.default.nativeBuildInputs;
+              # buildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.default.buildInputs;
+              # Export compile commands JSON for LSP and other tools
+              shellHook = ''
+                mkdir --verbose build
+                cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -GNinja -S . -B build
+              '';
+            };
+      });
     };
 }
