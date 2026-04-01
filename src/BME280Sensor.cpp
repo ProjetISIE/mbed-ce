7d6482deef90bba4063b0c60ad134c7b3bf3cac9
Guilhem Fauré <pro@guilhemfau.re>
2026-04-01 14:03:21 +0200

    fix: i2c reading conflict

--- src/BME280Sensor.cpp
+++ src/BME280Sensor.cpp
@@ -7,15 +7,15 @@ bool BME280Sensor::init() {
   bool found = false;
 
   for (int a : addrs) {
-    char cmd = 0xD0;
-    char id = 0;
+    uint8_t cmd = 0xD0;
+    uint8_t id = 0;
     int r_wr = 1, r_rd = 1;
 
     for (int i = 0; i < 3; i++) {
-      r_wr = _i2c.write(a, &cmd, 1);
+      r_wr = _i2c.write(a, (const char *)&cmd, 1);
       if (r_wr == 0) {
         wait_us(1000); // Settling delay
-        r_rd = _i2c.read(a, &id, 1);
+        r_rd = _i2c.read(a, (char *)&id, 1);
         if (r_rd == 0)
           break;
       }
@@ -37,52 +37,52 @@ bool BME280Sensor::init() {
     return false;
 
   // Read compensation parameters
-  char data[32];
-  char cmd_cal = 0x88;
-  if (_i2c.write(_address, &cmd_cal, 1) == 0) {
+  uint8_t data[32];
+  uint8_t cmd_cal = 0x88;
+  if (_i2c.write(_address, (const char *)&cmd_cal, 1) == 0) {
     wait_us(1000);
-    if (_i2c.read(_address, data, 24) != 0)
+    if (_i2c.read(_address, (char *)data, 24) != 0)
       return false;
   } else {
     return false;
   }
 
   _comp.dig_T1 = (data[1] << 8) | data[0];
-  _comp.dig_T2 = (data[3] << 8) | data[2];
-  _comp.dig_T3 = (data[5] << 8) | data[4];
+  _comp.dig_T2 = (int16_t)((data[3] << 8) | data[2]);
+  _comp.dig_T3 = (int16_t)((data[5] << 8) | data[4]);
 
-  char cmd_h1 = 0xA1;
-  if (_i2c.write(_address, &cmd_h1, 1) == 0) {
+  uint8_t cmd_h1 = 0xA1;
+  if (_i2c.write(_address, (const char *)&cmd_h1, 1) == 0) {
     wait_us(1000);
-    if (_i2c.read(_address, data, 1) != 0)
+    if (_i2c.read(_address, (char *)data, 1) != 0)
       return false;
   } else {
     return false;
   }
   _comp.dig_H1 = data[0];
 
-  char cmd_h2 = 0xE1;
-  if (_i2c.write(_address, &cmd_h2, 1) == 0) {
+  uint8_t cmd_h2 = 0xE1;
+  if (_i2c.write(_address, (const char *)&cmd_h2, 1) == 0) {
     wait_us(1000);
-    if (_i2c.read(_address, data, 7) != 0)
+    if (_i2c.read(_address, (char *)data, 7) != 0)
       return false;
   } else {
     return false;
   }
-  _comp.dig_H2 = (data[1] << 8) | data[0];
+  _comp.dig_H2 = (int16_t)((data[1] << 8) | data[0]);
   _comp.dig_H3 = data[2];
-  _comp.dig_H4 = (data[3] << 4) | (data[4] & 0x0F);
-  _comp.dig_H5 = (data[5] << 4) | (data[4] >> 4);
-  _comp.dig_H6 = data[6];
+  _comp.dig_H4 = (int16_t)((data[3] << 4) | (data[4] & 0x0F));
+  _comp.dig_H5 = (int16_t)((data[5] << 4) | (data[4] >> 4));
+  _comp.dig_H6 = (int8_t)data[6];
 
-  char config[2];
+  uint8_t config[2];
   config[0] = 0xF2;
-  config[1] = 0x01; // ctrl_hum
-  _i2c.write(_address, config, 2);
+  config[1] = 0x01; // ctrl_hum: osrs_h x1
+  _i2c.write(_address, (const char *)config, 2);
   wait_us(1000);
   config[0] = 0xF4;
-  config[1] = 0x27; // ctrl_meas
-  _i2c.write(_address, config, 2);
+  config[1] = 0x27; // ctrl_meas: osrs_t x1, osrs_p x1, mode normal
+  _i2c.write(_address, (const char *)config, 2);
 
   thread_sleep_for(20);
   return true;
@@ -92,14 +92,14 @@ bool BME280Sensor::read(float &temp, float &hum) {
   if (_address == 0)
     return false;
 
-  char cmd[1] = {0xF7};
-  char data[8];
+  uint8_t cmd = 0xF7;
+  uint8_t data[8];
 
   bool success = false;
   for (int i = 0; i < 3; i++) {
-    if (_i2c.write(_address, cmd, 1) == 0) {
+    if (_i2c.write(_address, (const char *)&cmd, 1) == 0) {
       wait_us(1000);
-      if (_i2c.read(_address, data, 8) == 0) {
+      if (_i2c.read(_address, (char *)data, 8) == 0) {
         success = true;
         break;
       }
@@ -110,20 +110,21 @@ bool BME280Sensor::read(float &temp, float &hum) {
   if (!success)
     return false;
 
-  int32_t adc_T = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
-  int32_t adc_H = (data[6] << 8) | data[7];
+  uint32_t adc_T = ((uint32_t)data[3] << 12) | ((uint32_t)data[4] << 4) |
+                   ((uint32_t)data[5] >> 4);
+  uint32_t adc_H = ((uint32_t)data[6] << 8) | (uint32_t)data[7];
 
-  if (adc_T == 0 && adc_H == 0) {
+  if (adc_T == 0 || adc_T == 0x80000) {
     return false;
   }
 
   // Temperature compensation
   int32_t var1, var2;
-  var1 = ((((adc_T >> 3) - ((int32_t)_comp.dig_T1 << 1))) *
+  var1 = ((((int32_t)(adc_T >> 3) - ((int32_t)_comp.dig_T1 << 1))) *
           ((int32_t)_comp.dig_T2)) >>
          11;
-  var2 = (((((adc_T >> 4) - ((int32_t)_comp.dig_T1)) *
-            ((adc_T >> 4) - ((int32_t)_comp.dig_T1))) >>
+  var2 = (((((int32_t)(adc_T >> 4) - ((int32_t)_comp.dig_T1)) *
+            ((int32_t)(adc_T >> 4) - ((int32_t)_comp.dig_T1))) >>
            12) *
           ((int32_t)_comp.dig_T3)) >>
          14;
@@ -133,7 +134,7 @@ bool BME280Sensor::read(float &temp, float &hum) {
   // Humidity compensation
   int32_t v_x1_u32r;
   v_x1_u32r = (_comp.t_fine - ((int32_t)76800));
-  v_x1_u32r = (((((adc_H << 14) - (((int32_t)_comp.dig_H4) << 20) -
+  v_x1_u32r = (((((((int32_t)adc_H) << 14) - (((int32_t)_comp.dig_H4) << 20) -
                   (((int32_t)_comp.dig_H5) * v_x1_u32r)) +
                  ((int32_t)16384)) >>
                 15) *
@@ -151,9 +152,6 @@ bool BME280Sensor::read(float &temp, float &hum) {
   v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
   v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
   hum = (float)(v_x1_u32r >> 12) / 1024.0f;
-
-  if (temp == 0.0f && hum == 0.0f)
-    return false;
 
   return true;
 }
--- src/TH02Sensor.cpp
+++ src/TH02Sensor.cpp
@@ -3,15 +3,15 @@
 TH02Sensor::TH02Sensor(I2C &i2c) : _i2c(i2c) {}
 
 bool TH02Sensor::init() {
-  char cmd = 0x11; // REG_ID
-  char data = 0;
+  uint8_t cmd = 0x11; // REG_ID
+  uint8_t data = 0;
   int r_wr = 1, r_rd = 1;
 
   for (int i = 0; i < 3; i++) {
-    r_wr = _i2c.write(ADDR, &cmd, 1);
+    r_wr = _i2c.write(ADDR, (const char *)&cmd, 1);
     if (r_wr == 0) {
       wait_us(1000); // Settling delay
-      r_rd = _i2c.read(ADDR, &data, 1);
+      r_rd = _i2c.read(ADDR, (char *)&data, 1);
       if (r_rd == 0)
         break;
     }
@@ -20,12 +20,14 @@ bool TH02Sensor::init() {
 
   printf("[TH02] Init: write_ret=%d, read_ret=%d, ID=0x%02X\n", r_wr, r_rd,
          data);
+  // Some TH02 sensors return 0x50, some other values. We'll be lenient if it
+  // responds.
   return (r_wr == 0 && r_rd == 0);
 }
 
 bool TH02Sensor::read(float &temp, float &hum) {
-  char cmd[2];
-  char data[3];
+  uint8_t cmd[2];
+  uint8_t data[2];
 
   // Measure Temperature
   cmd[0] = 0x03; // REG_CONFIG
@@ -33,11 +35,11 @@ bool TH02Sensor::read(float &temp, float &hum) {
 
   bool success = false;
   for (int i = 0; i < 3; i++) {
-    if (_i2c.write(ADDR, cmd, 2) == 0) {
+    if (_i2c.write(ADDR, (const char *)cmd, 2) == 0) {
       success = true;
       break;
     }
-    thread_sleep_for(10);
+    thread_sleep_for(20);
   }
   if (!success) {
     printf("[TH02] Read: Write config (temp) failed after retries\n");
@@ -46,12 +48,13 @@ bool TH02Sensor::read(float &temp, float &hum) {
 
   bool ready = false;
   for (int i = 0; i < 20; i++) {
-    thread_sleep_for(20);
-    cmd[0] = 0x00; // REG_STATUS
-    if (_i2c.write(ADDR, cmd, 1) == 0) {
+    thread_sleep_for(30);
+    uint8_t status_reg = 0x00; // REG_STATUS
+    if (_i2c.write(ADDR, (const char *)&status_reg, 1) == 0) {
       wait_us(1000);
-      if (_i2c.read(ADDR, data, 1) == 0) {
-        if (!(data[0] & 0x01)) {
+      uint8_t status = 0;
+      if (_i2c.read(ADDR, (char *)&status, 1) == 0) {
+        if (!(status & 0x01)) {
           ready = true;
           break;
         }
@@ -63,10 +66,10 @@ bool TH02Sensor::read(float &temp, float &hum) {
     return false;
   }
 
-  cmd[0] = 0x01; // REG_DATA_H
-  if (_i2c.write(ADDR, cmd, 1) == 0) {
+  uint8_t data_reg = 0x01; // REG_DATA_H
+  if (_i2c.write(ADDR, (const char *)&data_reg, 1) == 0) {
     wait_us(1000);
-    if (_i2c.read(ADDR, data, 3) != 0) {
+    if (_i2c.read(ADDR, (char *)data, 2) != 0) {
       printf("[TH02] Read: Read data (temp) failed\n");
       return false;
     }
@@ -74,7 +77,7 @@ bool TH02Sensor::read(float &temp, float &hum) {
     return false;
   }
 
-  uint16_t temp_raw = (data[1] << 8) | data[2];
+  uint16_t temp_raw = (data[0] << 8) | data[1];
   temp = (float)(temp_raw >> 2) / 32.0f - 50.0f;
 
   // Measure Humidity
@@ -82,11 +85,11 @@ bool TH02Sensor::read(float &temp, float &hum) {
   cmd[1] = 0x01; // CMD_MEASURE_HUMI
   success = false;
   for (int i = 0; i < 3; i++) {
-    if (_i2c.write(ADDR, cmd, 2) == 0) {
+    if (_i2c.write(ADDR, (const char *)cmd, 2) == 0) {
       success = true;
       break;
     }
-    thread_sleep_for(10);
+    thread_sleep_for(20);
   }
   if (!success) {
     printf("[TH02] Read: Write config (hum) failed after retries\n");
@@ -95,12 +98,13 @@ bool TH02Sensor::read(float &temp, float &hum) {
 
   ready = false;
   for (int i = 0; i < 20; i++) {
-    thread_sleep_for(20);
-    cmd[0] = 0x00; // REG_STATUS
-    if (_i2c.write(ADDR, cmd, 1) == 0) {
+    thread_sleep_for(30);
+    uint8_t status_reg = 0x00; // REG_STATUS
+    if (_i2c.write(ADDR, (const char *)&status_reg, 1) == 0) {
       wait_us(1000);
-      if (_i2c.read(ADDR, data, 1) == 0) {
-        if (!(data[0] & 0x01)) {
+      uint8_t status = 0;
+      if (_i2c.read(ADDR, (char *)&status, 1) == 0) {
+        if (!(status & 0x01)) {
           ready = true;
           break;
         }
@@ -112,10 +116,10 @@ bool TH02Sensor::read(float &temp, float &hum) {
     return false;
   }
 
-  cmd[0] = 0x01; // REG_DATA_H
-  if (_i2c.write(ADDR, cmd, 1) == 0) {
+  data_reg = 0x01; // REG_DATA_H
+  if (_i2c.write(ADDR, (const char *)&data_reg, 1) == 0) {
     wait_us(1000);
-    if (_i2c.read(ADDR, data, 3) != 0) {
+    if (_i2c.read(ADDR, (char *)data, 2) != 0) {
       printf("[TH02] Read: Read data (hum) failed\n");
       return false;
     }
@@ -123,13 +127,8 @@ bool TH02Sensor::read(float &temp, float &hum) {
     return false;
   }
 
-  uint16_t humi_raw = (data[1] << 8) | data[2];
+  uint16_t humi_raw = (data[0] << 8) | data[1];
   hum = (float)(humi_raw >> 4) / 16.0f - 24.0f;
-
-  if (temp == 0.0f && hum == 0.0f) {
-    printf("[TH02] Read: All zeros received\n");
-    return false;
-  }
 
   return true;
 }
